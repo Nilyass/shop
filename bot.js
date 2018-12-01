8b4bb045ed85da24ac3fec90693623b0c6504045
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-01 17:04:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,40 +14,17 @@ client.on('message', message => {
   if (message.author.id ==461059496861630465) return;
 
 
-if (message.content.startsWith(prefix + 'playing')) {
-if (message.author.id !== '407695930750402561','294980175253209089') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
-} 
+client.on('ready', () => {
+   console.log(----------------);
+      console.log(Desert Bot- Script By : EX Clan);
+        console.log(----------------);
+      console.log(ON ${client.guilds.size} Servers '     Script By : EX Clan ');
+    console.log(----------------);
+  console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(Wolf Shop Bot,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
 
- 
-if (message.content.startsWith(prefix + 'streem')) {
-if (message.author.id !== '407695930750402561','294980175253209089') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
-    message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
-} else
-
-if (message.content.startsWith(prefix + 'name')) {
-if (message.author.id !== '407695930750402561','294980175253209089') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
-  return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
-} else
-    
-if (message.content.startsWith(prefix + 'image')) {
-if (message.author.id !== '407695930750402561','294980175253209089') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-client.user.setAvatar(argresult);
-    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-} else
-
-
-if (message.content.startsWith(prefix + 'watching')) {
-if (message.author.id !== '407695930750402561','294980175253209089') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-    client.user.setActivity(argresult, {type : 'watching'});
- message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
-}
-
- });
  
  client.login(process.env.BOT_TOKEN);
  
