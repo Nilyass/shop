fa80be19d1d6fd4c0c1d91945fb8bea19cf3a861
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-01 16:39:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id == 444182215476248576) return;
+  if (message.author.id ==461059496861630465) return;
 
 
 if (message.content.startsWith(prefix + 'playing')) {
